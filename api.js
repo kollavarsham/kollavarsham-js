9d85858b7d47c2d6c81f6e377bfc12b92c37efcd
Floyd Pink <imbleedingme@googlemail.com>
2015-09-17 10:14:00 -0400

    Update 2015-09-17T14:13:56.540Z

--- api.js
+++ api.js
@@ -7,19 +7,31 @@ YUI.add("yuidoc-meta", function(Y) {
         "KollavarshamDate"
     ],
     "modules": [
-        "date",
-        "kollavarsham"
+        "baseDate",
+        "julianDate",
+        "kollavarsham",
+        "kollavarshamDate"
     ],
     "allModules": [
         {
-            "displayName": "date",
-            "name": "date",
-            "description": "Defines the {{#crossLink \"KollavarshamDate\"}}{{/crossLink}} and {{#crossLink \"JulianDate\"}}{{/crossLink}} classes"
+            "displayName": "baseDate",
+            "name": "baseDate",
+            "description": "Defines the {{#crossLink \"BaseDate\"}}{{/crossLink}} which is inherited by {{#crossLink \"KollavarshamDate\"}}{{/crossLink}} and {{#crossLink \"JulianDate\"}}{{/crossLink}} classes"
+        },
+        {
+            "displayName": "julianDate",
+            "name": "julianDate",
+            "description": "Defines the {{#crossLink \"JulianDate\"}}{{/crossLink}} class"
         },
         {
             "displayName": "kollavarsham",
             "name": "kollavarsham",
             "description": "The main module that exposes all of the public API in the library"
+        },
+        {
+            "displayName": "kollavarshamDate",
+            "name": "kollavarshamDate",
+            "description": "Defines the {{#crossLink \"KollavarshamDate\"}}{{/crossLink}} class"
         }
     ],
     "elements": []
